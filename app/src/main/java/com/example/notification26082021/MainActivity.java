ee1c8a69664bfe1eee01fe379d5d9e1d95677a24
phamtanphat <phatdroid94@gmail.com>
2021-10-30 20:40:07 +0700

    ui basic

--- app/src/main/java/com/example/notification26082021/MainActivity.java
+++ app/src/main/java/com/example/notification26082021/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.notification26082021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.notification26082021.databinding.ActivityMainBinding;
 
@@ -14,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+
+        mBinding.buttonOpenNotifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"CHANNEL_ID");
+                builder.setSmallIcon(android.R.drawable.ic_dialog_email);
+                builder.setShowWhen(true);
+                builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),android.R.drawable.ic_dialog_email));
+                builder.setContentTitle("Thông báo");
+                builder.setContentText("Ứng dụng có phiên bản cập nhật mới!!");
+
+            }
+        });
     }
 }
